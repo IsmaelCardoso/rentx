1276c3215f9f7e1090b27965bd0974e8427adf37
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-01-17 11:30:41 -0300

    chore(Init project):  - Created tsconfig.json  - Change App for .tsx  - Screen (Home): created screen and styled  - Install and config Fonts  - Config global thems in styled-components

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Title } from './Home.styles';
+
+const Home = () => {
+    return (
+        <Container>
+            <Title>Title</Title>
+        </Container>
+    );
+}
+export default Home;
