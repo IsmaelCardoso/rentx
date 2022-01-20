cca244ebe1c9f0cfa10aac3455c3625a1fa184e3
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-01-20 10:12:22 -0300

    chore(CarDetails):  - Created session brand and price  - Created session about

--- src/screens/CarDetails/CarDetails.tsx
+++ src/screens/CarDetails/CarDetails.tsx
@@ -8,18 +8,49 @@ import {
     Container, 
     Header,
     CarImageContainer,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About,
 } from './CarDetails.styles';
 
 const CarDetails = () => {
     return (
         <Container>
             <Header>
-                <BackButton onPress={() => {}} />
+              <BackButton onPress={() => {}} />
             </Header>
 
             <CarImageContainer>
-                <ImageSlider imagesUrl={["https://beta.alpes.one/storage/app/uploads/public/608/ad6/90f/608ad690f418e968296549.png"]} />
+              <ImageSlider imagesUrl={["https://beta.alpes.one/storage/app/uploads/public/608/ad6/90f/608ad690f418e968296549.png"]} />
             </CarImageContainer>
+
+            <Content>
+              <Details>
+                <Description>
+                    <Brand>Lamborghini</Brand>
+                    <Name>Huracan</Name>
+                </Description>
+
+                <Rent>
+                    <Period>Ao dia</Period>
+                    <Price>R$ 580</Price>
+                </Rent>
+              </Details>
+
+
+
+              <About>
+                Este é automóvel desportivo. Surgiu do lendário 
+                touro de lide indultado na praça Real Maestranza de Sevilla. 
+                É um belíssimo carro para quem gosta de acelerar.
+              </About>
+            </Content>
         </Container>
     );
 }
