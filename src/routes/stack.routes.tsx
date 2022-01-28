1e6851b72834bd8db83d0e9d83c062ff950e1668
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-01-28 11:53:36 -0300

    chore(Splash):  - Created Splash screen  - stack.routes: Config Splash ascreen  - Install: react-native-reanimated by expo  - Config babel.config

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import Scheduling from '../screens/Scheduling';
 import SchedulingDetails from '../screens/SchedulingDetails';
 import SchedulingComplete from '../screens/SchedulingComplete';
 import MyCars from '../screens/MyCars';
+import Splash from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator()
 
@@ -17,7 +18,13 @@ const StackRoutes = () => {
       screenOptions={{
         headerShown: false,
       }}
+      initialRouteName='Splash'
     >
+      <Screen
+        name="Splash"
+        component={Splash}
+      />
+
       <Screen
         name="Home"
         component={Home}
---
+++ src/screens/Splash/Splash.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from './Splash.styles';
+
+const Splash = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default Splash;
