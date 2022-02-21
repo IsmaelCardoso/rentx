b12041a7e1460a94236cfa801915dc54a15cb0ff
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-02-20 23:13:58 -0300

    chore(Auth and Navigation):  - New PNGs  - app.tab.routes: Set new PNGs and styles

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,33 +1,65 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
+
+import HomeSVG from '../assets/home.svg';
+import CarSVG from '../assets/car.svg';
+import PeopleSVG from '../assets/people.svg';
 
 import AppStackRoutes from './app.stack.routes'
+
 import Home from '../screens/Home';
 import MyCars from '../screens/MyCars';
+
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 const AppTabRoutes = () => {
+  const theme = useTheme()
 
   return (
     <Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.text_detail,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary,
+        }
       }}
     >
       <Screen
         name="Home"
         component={AppStackRoutes}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <HomeSVG width={24} height={24} fill={color}/>
+          ))
+        }}
       />
 
       <Screen
         name="Profile"
         component={Home}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <PeopleSVG width={24} height={24} fill={color} />
+          ))
+        }}
       />
 
       <Screen
         name="MyCars"
         component={MyCars}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <CarSVG width={24} height={24} fill={color} />
+          ))
+        }}
       />
     </Navigator>
   )
